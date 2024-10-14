acc65a4c25e5e1fd2f99008d718433cc67f55164
Yohann Bacha <github@cyberendroit.net>
2024-10-14 09:45:25 +0200

    ajout d'une ci (#5)

--- src/routes/profil/+page.svelte
+++ src/routes/profil/+page.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-  import { pb } from '$lib/database';
   import { currentUser } from '$lib/login.svelte';
-
-  pb.collection('users').authRefresh();
 </script>
 
 <div class="p-2">
@@ -10,15 +7,15 @@
   <div class="flex flex-col gap-4">
     <div class="flex flex-col">
       <span class="text-xs font-bold">Nom</span>
-      <span>{currentUser.value.name}</span>
+      <span>{currentUser.value?.name}</span>
     </div>
     <div class="flex flex-col">
       <span class="text-xs font-bold">Pseudonyme</span>
-      <span>{currentUser.value.username}</span>
+      <span>{currentUser.value?.username}</span>
     </div>
     <div class="flex flex-col">
       <span class="text-xs font-bold">Adresse mail</span>
-      <span>{currentUser.value.email}</span>
+      <span>{currentUser.value?.email}</span>
     </div>
   </div>
 </div>
